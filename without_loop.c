420b8fbd6a62f0c0f8c1938caffa79e15ae69aaa
AswinBalan <aswindpsp15@gmail.com>
2019-01-20 21:33:21 +0530

    recursion

---
+++ without_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+  static int flag = 0, iter = 1, limit;
+  if (flag == 0)
+    {
+        printf("Enter the limit:\t");
+        scanf("%d",&limit);
+        printf("The numbers upto the limit:\n");
+        flag = 1;
+    }
+    //checking the exit condition
+    if ( iter-1 == limit)
+        return ;
+  printf("%d\n",iter++);
+  main();
+}
